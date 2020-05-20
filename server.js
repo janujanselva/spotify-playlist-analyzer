07991cade4151bda09e7880550dc864abda27b97
Janujan <janujan.selva@gmail.com>
2020-05-20 14:18:03 -0400

    stuff

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-var gzippo = require('gzippo');
+var port = Number(process.env.PORT || 8000);
+
 var express = require('express');
-var morgan = require('morgan');
 var app = express();
 
-app.use(morgan('dev'));
-app.use(gzippo.staticGzip("" + __dirname + "/"));
-app.get('/', function(req, res) {
-    res.sendfile('app/view1/view1.html', {root: __dirname })
+app.use(express.static(__dirname + '/app'));
+var server = app.listen(port, function() {
+    console.log('Listening on port %d', server.address().port);
 });
-var server = app.listen(process.env.PORT || 80);
